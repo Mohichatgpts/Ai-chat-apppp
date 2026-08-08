9cd9983be7b9aa7222d3923255730712142ab929
Mohichatgpts <gikih68405@davopa.com>
2026-08-08 13:49:18 +0530

    Refactor server setup and improve socket handling

--- server.js
+++ server.js
@@ -1,70 +1,58 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-// Bina public folder ke index.html aur doosri files load karne ke liye
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(path.join(__dirname)));
 
 let chatHistory = [];
-let connectedUsers = 0;
-let roomTitle = "Friends 😇😊";
+let roomTitle = "Friends😇🙂";
 
 io.on('connection', (socket) => {
-    connectedUsers++;
-    
-    socket.emit('title_updated', roomTitle);
-    socket.emit('load_history', chatHistory);
+    socket.emit('title_updated', roomTitle);
+    socket.emit('load_history', chatHistory);
+    socket.broadcast.emit('chat_start');
 
-    if (connectedUsers >= 2) {
-        io.emit('chat_start');
-    } else {
-        socket.emit('waiting⏳ for 2nd friend');
-    }
+    socket.on('change_title', (newTitle) => {
+        roomTitle = newTitle;
+        io.emit('title_updated', roomTitle);
+    });
 
-    socket.on('change_title', (newTitle) => {
-        roomTitle = newTitle;
-        io.emit('title_updated', roomTitle);
-    });
+    socket.on('send_message', (data) => {
+        chatHistory.push(data);
+        socket.broadcast.emit('receive_message', data);
+    });
 
-    socket.on('send_message', (data) => {
-        chatHistory.push(data);
-        socket.broadcast.emit('receive_message', data);
-    });
+    socket.on('send_image', (data) => {
+        chatHistory.push(data);
+        socket.broadcast.emit('receive_image', data);
+    });
 
-    socket.on('send_image', (data) => {
-        chatHistory.push(data);
-        socket.broadcast.emit('receive_image', data);
-    });
+    socket.on('send_audio', (data) => {
+        chatHistory.push(data);
+        socket.broadcast.emit('receive_audio', data);
+    });
 
-    socket.on('send_audio', (data) => {
-        chatHistory.push(data);
-        socket.broadcast.emit('receive_audio', data);
-    });
+    socket.on('typing', () => {
+        socket.broadcast.emit('display_typing');
+    });
 
-    socket.on('typing', () => {
-        socket.broadcast.emit('display_typing');
-    });
+    socket.on('stop_typing', () => {
+        socket.broadcast.emit('hide_typing');
+    });
 
-    socket.on('stop_typing', () => {
-        socket.broadcast.emit('hide_typing');
-    });
+    // REAL-TIME REACTION SYNC
+    socket.on('send_reaction', (data) => {
+        socket.broadcast.emit('receive_reaction', data);
+    });
 
-    socket.on('disconnect', () => {
-        connectedUsers--;
-        io.emit('user_left');
-    });
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user_left');
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
