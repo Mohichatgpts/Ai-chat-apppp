e9a3332dd0bd67161a8b67de18a3e953754443fe
Mohichatgpts <gikih68405@davopa.com>
2026-08-08 16:54:36 +0530

    Refactor socket event handlers for clarity and efficiency

--- server.js
+++ server.js
@@ -20,38 +20,34 @@ io.on('connection', (socket) => {
     });
 
     socket.on('send_message', (data) => {
+        data.deletedFor = [];
         chatHistory.push(data);
         socket.broadcast.emit('receive_message', data);
     });
 
     socket.on('send_image', (data) => {
+        data.deletedFor = [];
         chatHistory.push(data);
         socket.broadcast.emit('receive_image', data);
     });
 
     socket.on('send_audio', (data) => {
+        data.deletedFor = [];
         chatHistory.push(data);
         socket.broadcast.emit('receive_audio', data);
     });
 
-    socket.on('typing', () => {
-        socket.broadcast.emit('display_typing');
-    });
-
-    socket.on('stop_typing', () => {
-        socket.broadcast.emit('hide_typing');
-    });
+    socket.on('typing', () => socket.broadcast.emit('display_typing'));
+    socket.on('stop_typing', () => socket.broadcast.emit('hide_typing'));
 
     socket.on('send_reaction', (data) => {
         const msg = chatHistory.find(m => m.id === data.msgId);
-        if (msg) {
-            msg.reaction = data.emoji;
-        }
+        if (msg) msg.reaction = data.emoji;
         socket.broadcast.emit('receive_reaction', data);
     });
 
-    // DELETE FOR EVERYONE LOGIC
-    socket.on('delete_message', (data) => {
+    // DELETE FOR EVERYONE
+    socket.on('delete_message_everyone', (data) => {
         const index = chatHistory.findIndex(m => m.id === data.msgId);
         if (index !== -1) {
             chatHistory[index].deleted = true;
@@ -60,15 +56,36 @@ io.on('connection', (socket) => {
             delete chatHistory[index].audio;
             delete chatHistory[index].reaction;
         }
-        io.emit('message_deleted', { msgId: data.msgId });
+        io.emit('message_deleted_everyone', { msgId: data.msgId });
     });
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user_left');
+    // DELETE FOR ME (RAM सफाई 2 यूज़र्स के लिए)
+    socket.on('delete_message_for_me', (data) => {
+        const msg = chatHistory.find(m => m.id === data.msgId);
+        if (msg) {
+            if (!msg.deletedFor) msg.deletedFor = [];
+            if (!msg.deletedFor.includes(data.userId)) {
+                msg.deletedFor.push(data.userId);
+            }
+            if (msg.deletedFor.length >= 2) {
+                chatHistory = chatHistory.filter(m => m.id !== data.msgId);
+            }
+        }
     });
+
+    // CLEAR CHAT FOR ME
+    socket.on('clear_chat_for_me', (data) => {
+        chatHistory.forEach(msg => {
+            if (!msg.deletedFor) msg.deletedFor = [];
+            if (!msg.deletedFor.includes(data.userId)) {
+                msg.deletedFor.push(data.userId);
+            }
+        });
+        chatHistory = chatHistory.filter(msg => msg.deletedFor.length < 2);
+    });
+
+    socket.on('disconnect', () => socket.broadcast.emit('user_left'));
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
